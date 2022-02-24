c8cba1dd813261630fad501e2f9dd5e1fb178352
Sadman Sakib <34483142+sadmansakib811@users.noreply.github.com>
2022-02-24 12:13:42 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,456 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+div.gallery {
+    border: 1px solid #ccc;
+}
+
+div.gallery:hover {
+    border: 1px solid #777;
+}
+
+div.gallery img {
+    width: 100%;
+    height: auto;
+}
+
+div.desc {
+    padding: 15px;
+    text-align: center;
+}
+
+* {
+    box-sizing: border-box;
+}
+
+.responsive {
+    padding: 0 6px;
+    float: left;
+    width: 24.99999%;
+}
+
+@media only screen and (max-width: 700px) {
+    .responsive {
+        width: 49.99999%;
+        margin: 6px 0;
+    }
+}
+
+@media only screen and (max-width: 500px) {
+    .responsive {
+        width: 100%;
+    }
+}
+
+.clearfix:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+</style>
+
+
+
+<title></title>
+
+<link href="css/owl.carousel.css" rel='stylesheet' type='text/css'/>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+ //function hideURLbar(){ window.scrollTo(0,1); } 
+ 
+ 
+ </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+<!--/js-->
+<!--animated-css-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<!--/animated-css-->
+</head>
+<body>
+<!--header-->
+<!--sticky-->
+<?php include('function.php'); ?>
+
+<?php include('top.php'); ?>
+<!--/sticky-->
+<?php include('slider.php'); ?>
+
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+ 
+	  
+	  
+	  
+	  
+	  
+	  
+<!--About-->
+     <div class="about section" id="section-2">
+	  <div class="about-head text-center">
+	  <h3>About Us</h3>
+	  <span></span><img src="images/about-img.png" alt=""><span></span>
+	  </div>
+	   <div class="container">		  
+		 <div class="col-md-4 about-grids">
+			 <h4><span class="icon1"></span>Our Vision</h4>
+			 <p>Tourism which is ethical, fair and a positive experience for both travellers and the people and places they visit</p>
+		 </div>
+		 <div class="col-md-4 about-grids grid2">
+			 <h4><span class="icon2"></span>Our Mission</h4>
+			 <p>To ensure tourism always benefits local people by challenging bad practice and promoting better tourism</p>
+		 </div>
+		 <div class="col-md-4 about-grids">
+			 <h4><span class="icon3"></span>Safety Information</h4>
+			 <p>Vacation is a time to relax in safe surroundings.For emergency aid of any kind, call 911 from any phone... in your hotel, dial 9-911.</p>
+             <div align="right"><a href="aboutus.php"><input type="button" value="Read More" name="sbmt" /></a></div> 
+		 </div>
+	 </div>
+</div>
+<!--/About-->
+<!--top-tours-->	
+<div  class="section" id="section-3">
+
+ <div class="top-tours-head text-center">
+		  <h3>Gallery</h3>
+		  <span></span><img src="images/star.png" alt=""><span></span>
+		   </div>
+		   
+		   <div style="padding:10px;">
+
+  <div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd1.jpg">
+      <img src="images/bd1.jpg" alt="Cinque Terre" width="600" height="400">
+    </a>
+    <div class="desc">Srimongol,Shylet</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd2.jpg">
+      <img src="images/bd2.jpg" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">Shorupkathi Flooded Guava Market,Barisal</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd3.jpg">
+      <img src="images/bd3.jpg" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">Sadarghat launch terminal</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd4.jpg">
+      <img src="images/bd4.jpg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Nilgiri</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd5.jpg">
+      <img src="images/bd5.jpg" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">shajek</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd6.jpg">
+      <img src="images/bd6.jpg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Sonargaon</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd7.jpg">
+      <img src="images/bd7.jpg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Coxz Bazar</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/bd9.jpg">
+      <img src="images/bd9.jpg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Saint martin</div>
+  </div>
+</div>
+
+
+<div class="clearfix"></div>
+
+<div style="padding:6px;">
+  
+  
+  
+  
+  
+  
+   
+   
+  
+
+
+</div>
+
+	
+
+<!-- Script for gallery Here-->
+
+<!--Gallery Script Ends-->	 
+<!--/top-tours-->
+
+
+<div class="tour-guides section" id="section-4" >
+	  <div class="tour-guides-head text-center">
+		  <h3>Advertisements</h3>
+		  <span></span><img src="images/guide.png" alt=""><span></span>
+		  <div class="container">
+				<p>My Tour guides are expensive, so first decide what you want to do, whether it's a general tour of a city, a day of hiking, or visiting remote villages in a third-world country. Have a ball-park time allotment in mind, whether it's a half-day, a full-day, or a multi-day experience. Once you have chosen a guide, you'll probably refine your ideas based upon the guide's knowledge.</p>
+		  </div>
+	  </div>
+	  <div class="container">
+		<!-- requried-jsfiles-for owl -->
+				<link href="css/owl.carousel.css" rel="stylesheet">
+							    <script src="js/owl.carousel.js"></script>
+							        <script>
+							    $(document).ready(function() {
+							      $("#owl-demo").owlCarousel({
+							        items : 1,
+							        lazyLoad : true,
+							        autoPlay : true,
+							        navigation : false,
+							        navigationText :  false,
+							        pagination : true,
+							      });
+							    });
+							    </script>
+			<!-- //requried-jsfiles-for owl -->
+		 <div id="owl-demo" class="owl-carousel">
+         
+         
+         
+         <?php
+$s="select * from advertisement";
+$result=mysqli_query($cn,$s);
+
+$n=0;
+while($data=mysqli_fetch_array($result))
+{
+	if($n%4==0)
+	{
+	?>
+         
+			  <div class="item text-center guide-sliders">
+<?php }?>
+
+
+	<div class="col-md-3 image-grid">
+					 <img src="Admin/addverimages/<?php echo $data[3]; ?>" width="300px" height="150px" >
+					
+					 <p><a href="#"><?php echo $data[2]; ?></a></p>
+				 </div>
+				<?php if($n%4==3)
+				{?>
+            </div>
+   
+            <?php
+				}
+			$n=$n+1;
+}
+?>
+            		  </div>
+		</div>
+</div>
+
+
+
+
+	
+<iframe width="100%" height="500" src="https://www.youtube.com/embed/svVIqd-XuGs?rel=0&amp;start=12" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+
+
+
+
+<div id="section-5" class="contact section">
+	  <div class="contact-head text-center">
+		  <h3>Contact Us</h3>
+		  <span></span><img src="images/mail.png" alt=""><span></span><br/><br/>
+          <h4 style="color:#000">Plan Your Trip
+Our travel experts can help you book now!</h4>
+
+		  <div class="contact-grids">
+			  <div class="container">
+				  <div class="col-md-4 address">
+						<h4 style="color:#09F">SHAILA HAMID Enterprises</h4>
+						<p style="color:#000">NEED HELP BOOKING PACKAGE<br/>
+                        For fantastic suggestions you may also call our travel expert</p>
+						<h5 style="color:#000"><span class="img1"></span>+8801918014886&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;(+91) 7696303090</h5>
+						<h5 style="color:#000"><span class="img2"></span><a href="https://www.facebook.com/shaila.hamidpolash" target="_blank">info[at]mytour.com&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;support[at]mytour.com</a></h5>
+						<h5 style="color:#000"><span class="img3"></span>House Building,Uttara,Dhaka.</h5>
+                        <br/>
+                       <img src="images/contac.jpg" />
+                      </div>
+				  <div class="col-md-8 contact">
+                  
+                  	<?php
+if(isset($_POST["sbmt"]))
+{
+	$cn=makeconnection();
+	$s="insert into contactus(Name,Phno,Email,Message) values('" . $_POST["t1"] ."','" . $_POST["t2"] ."','" . $_POST["t3"] ."','" . $_POST["t4"] ."')";
+	mysqli_query($cn,$s);
+	mysqli_close($cn);
+	echo "<script>alert('Record Save');</script>";
+}
+?>
+					  <form method="post">
+                      <table border="0" width="700px" height="500px">
+                      <tr><td align="left"> <input type="text" class="text" value=" Name"  name="t1" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Name';}" required pattern="[a-zA-z1 _]{1,50}" title"Please Enter Only Characters and numbers between 1 to 50 for Name"></td></tr>
+                      <tr><td align="left"><input type="text" class="text" value=" Contact No" name="t2" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Contact NO';}" required pattern="[0-9]{10,12}" title"Please Enter Only  numbers between 10 to 12 for Contact no"></td></tr>
+					 <tr><td align="left"> <input type="text" class="text" value="Email" name="t3" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your mail';}" required></td></tr>
+					 <tr><td><textarea onFocus="if(this.value == 'Message') this.value='';" name="t4" onBlur="if(this.value == '') this.value='Enter Message Here';" required/ >Message</textarea></td></tr>
+					  <tr><td><input type="submit" value="Send message" name="sbmt"></td></tr></table>
+					  <div class="clearfix"></div>
+					   </form>
+				   </div>
+				  <div class="clearfix"></div>
+			  </div>
+		  </div>
+	       
+
+	
+
+
+
+
+	
+		   
+		   
+	<div class=iframes>
+	
+
+<div class="top-tours-head text-center">
+		  <h3>Maps</h3>
+		  <span></span><img src="images/star.png" alt=""><span></span>
+		   
+
+<div id="map" style="  margin: auto;margin-top: 15vh;width:600px;height:400px;background:yellow;border:2px solid red">
+
+
+
+</div>
+
+<script>
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.12),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnQBKO8u37oLi3gmSwVJZyJPq5M4slxOk&callback=myMap"></script>
+ 
+</div>
+
+</div>
+		   
+		<div style="padding:40px;">   
+	   
+
+
+<?php include('bottom.php'); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<link href="https://snatchbot.me/sdk/webchat.css" rel="stylesheet" type="text/css"><script src="https://snatchbot.me/sdk/webchat.min.js"></script><script> Init('?botID=30319&appID=', 600, 600, 'https://dvgpba5hywmpo.cloudfront.net/media/image/GBsmau4YPV84dv33MY7Kz1o6p', 'bubble', '#00AFF0', 90, 90, 62.99999999999999, '', '1', '#FFFFFF', ); /* for authentication of its users, you can define your userID (add &userID={login}) */ </script>
+</body>
+</html>
+
